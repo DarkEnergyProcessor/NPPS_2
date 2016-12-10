f10940780fc9180f19174d6186f51be4cacec604
RayFirefist <matteodsi@gmail.com>
2016-12-10 15:57:07 +0100

    Updated some broken-arrays

---
+++ modules/include.const.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Null-Pointer Private Server
+ * Constants which related to SIF
+ */
+
+/// \file include.const.php
+
+/// Smile attribute
+define('NPPS_ATTRIBUTE_SMILE', 1);
+/// Pure attribute
+define('NPPS_ATTRIBUTE_PURE', 2);
+/// Cool attribute
+define('NPPS_ATTRIBUTE_COOL', 3);
+
+/// Song requires LP
+define('NPPS_CAPITAL_TYPE_ENERGY', 1);
+/// Song requires Token
+define('NPPS_CAPITAL_TYPE_TOKEN', 2);
+
+/// Easy difficulty
+define('NPPS_SONG_DIFFICULTY_EASY', 1);
+/// Normal difficuty
+define('NPPS_SONG_DIFFICULTY_NORMAL', 2);
+/// Hard difficulty
+define('NPPS_SONG_DIFFICULTY_HARD', 3);
+/// Expert difficulty
+define('NPPS_SONG_DIFFICULTY_EXPERT', 4);
+/// Random + Expert difficulty
+define('NPPS_SONG_DIFFICULTY_EXR', 5);
+/// Master difficulty
+define('NPPS_SONG_DIFFICULTY_MASTER', 6);
